914990cc8199824bab096cc7ecf2100d2357a4ed
ferrari_enzo <fmardonkulov@gmail.com>
2017-09-05 13:58:38 +0600

    rates page

---
+++ frontend/views/site/rates.php
@@ -0,0 +1,173 @@
+<div class="rates-page">
+    <div class="rates-content">
+        <div class="container">
+            <div class="row justify-content-center text-center">
+                <div class="col-md-7">
+                    <div class="title"><img class="img-inBlock" src="images/screen/screen-second-title.png" alt="Title"></div>
+                    <p class="text-white">Иногда, чтобы показать что-то важное, одной фотографии не достаточно. Поэтому мы придумали бози #bothie для тех, кто не любит ограничений. <br>
+                        Делай фото сразу с обеих камер с Nokia 8.</p>
+                    <p class="m-t-30 m-b-40 tt-up blue-text font-30">
+                        Не ограничивайся селфи. Делай бози <span class="tt-normal">#bothie</span>
+                    </p>
+                    <p class="text-white">Участвуй в нашем конкурсе и голосуй за лушие фотографии!</p>
+                    <div class="links">
+                        <div><a href="" class="simple-link">Как выиграть?</a></div>
+                        <div><a href="" class="border-link">Участвовать</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="container">
+                        <div id="container" class="bothie-blocks">
+                            <div class="item">
+                                <div class="bothie-vertical bothie-wrap">
+                                    <div class="wrap">
+                                        <div class="top bothie" style="background-image:url('images/bothie/bothie-3.jpg')"></div>
+                                        <div class="bottom bothie" style="background-image:url('images/bothie/bothie-3-2.jpg')"></div>
+                                        <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                        <span class="bothie-rat">1235</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item w2">
+                                <div class="bothie-vertical-wrap">
+                                    <div class="bothie-vertical bothie-wrap">
+                                        <div class="wrap">
+                                            <div class="top bothie" style="background-image:url('images/bothie/bothie-4.jpg')"></div>
+                                            <div class="bottom bothie" style="background-image:url('images/bothie/bothie-4-2.jpg')"></div>
+                                            <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                            <span class="bothie-rat">1235</span>
+                                        </div>
+                                    </div>
+                                    <div class="bothie-vertical bothie-wrap">
+                                        <div class="wrap">
+                                            <div class="top bothie" style="background-image:url('images/bothie/bothie-5-2.jpg')"></div>
+                                            <div class="bottom bothie" style="background-image:url('images/bothie/bothie-5.jpg')"></div>
+                                            <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                            <span class="bothie-rat">1235</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item text-right">
+                                <div class="bothie-vertical bothie-wrap">
+                                    <div class="wrap">
+                                        <div class="top bothie" style="background-image:url('images/bothie/bothie-6.jpg')"></div>
+                                        <div class="bottom bothie" style="background-image:url('images/bothie/bothie-6-2.jpg')"></div>
+                                        <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                        <span class="bothie-rat">1235</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="bothie-vertical bothie-wrap">
+                                    <div class="wrap">
+                                        <div class="top bothie" style="background-image:url('images/bothie/bothie-7.jpg')"></div>
+                                        <div class="bottom bothie" style="background-image:url('images/bothie/bothie-7-2.jpg')"></div>
+                                        <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                        <span class="bothie-rat">1235</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item w2">
+                                <div class="bothie-vertical-wrap">
+                                    <div class="bothie-vertical bothie-wrap">
+                                        <div class="wrap">
+                                            <div class="top bothie" style="background-image:url('images/bothie/bothie-8.jpg')"></div>
+                                            <div class="bottom bothie" style="background-image:url('images/bothie/bothie-8-2.jpg')"></div>
+                                            <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                            <span class="bothie-rat">1235</span>
+                                        </div>
+                                    </div>
+                                    <div class="bothie-vertical bothie-wrap">
+                                        <div class="wrap">
+                                            <div class="top bothie" style="background-image:url('images/bothie/bothie-9.jpg')"></div>
+                                            <div class="bottom bothie" style="background-image:url('images/bothie/bothie-9-2.jpg')"></div>
+                                            <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                            <span class="bothie-rat">1235</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item text-right">
+                                <div class="bothie-vertical bothie-wrap">
+                                    <div class="wrap">
+                                        <div class="top bothie" style="background-image:url('images/bothie/bothie-10.jpg')"></div>
+                                        <div class="bottom bothie" style="background-image:url('images/bothie/bothie-10-2.jpg')"></div>
+                                        <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                        <span class="bothie-rat">1235</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="bothie-vertical bothie-wrap">
+                                    <div class="wrap">
+                                        <div class="top bothie" style="background-image:url('images/bothie/bothie-11.jpg')"></div>
+                                        <div class="bottom bothie" style="background-image:url('images/bothie/bothie-11-2.jpg')"></div>
+                                        <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                        <span class="bothie-rat">1235</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item w2">
+                                <div class="bothie-vertical-wrap">
+                                    <div class="bothie-vertical bothie-wrap">
+                                        <div class="wrap">
+                                            <div class="top bothie" style="background-image:url('images/bothie/bothie-12-2.jpg')"></div>
+                                            <div class="bottom bothie" style="background-image:url('images/bothie/bothie-12.jpg')"></div>
+                                            <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                            <span class="bothie-rat">1235</span>
+                                        </div>
+                                    </div>
+                                    <div class="bothie-vertical bothie-wrap">
+                                        <div class="wrap">
+                                            <div class="top bothie" style="background-image:url('images/bothie/bothie-13.jpg')"></div>
+                                            <div class="bottom bothie" style="background-image:url('images/bothie/bothie-13-2.jpg')"></div>
+                                            <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                            <span class="bothie-rat">1235</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item text-right">
+                                <div class="bothie-vertical bothie-wrap">
+                                    <div class="wrap">
+                                        <div class="top bothie" style="background-image:url('images/bothie/bothie-14.jpg')"></div>
+                                        <div class="bottom bothie" style="background-image:url('images/bothie/bothie-14-2.jpg')"></div>
+                                        <a href="" class="bothie-btn rates-btn"><span>Like!</span></a>
+                                        <span class="bothie-rat">1235</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="rates-footer">
+        <div class="container">
+            <div class="links text-center">
+                <a href="" class="border-link load-more">Загрузить ещё</a>
+            </div>
+            <div class="row text-left">
+                <div class="col-md-2">
+                    <a href="">Условия обработки персональных данных</a>
+                </div>
+            </div>
+            <div class="shares text-center">
+                <div class="shares-title">
+                    <span>Нравится проект?</span>
+                </div>
+                <div class="shares-items">
+                    <ul>
+                        <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                        <li><a href=""><i class="fa fa-vk"></i></a></li>
+                        <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
